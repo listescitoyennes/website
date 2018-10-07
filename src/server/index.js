61856c0ee65a42c1ce16bd740eb116c0d4103d57
Xavier Damman <xdamman@gmail.com>
2018-10-07 14:27:18 +0200

    cleaning

--- src/server/index.js
+++ src/server/index.js
@@ -6,10 +6,8 @@ import express from 'express';
 import favicon from 'serve-favicon';
 import next from 'next';
 import { get } from 'lodash';
-import * as api from './api';
 import routes from '../routes';
 import logger from '../logger';
-import { titleCase } from '../lib/utils';
 import { registerToNewsletter } from '../lib/mailchimp';
 
 const {
@@ -39,88 +37,6 @@ nextApp.prepare().then(() => {
     const email = get(req, 'body.email');
     const newsletter = get(req, 'body.newsletter');
     registerToNewsletter(email, newsletter).then(data => res.json(data));
-  });
-
-  server.get('/api/data/:csvfile', api.getDataFromCSV);
-
-  const inc = (obj, key, increment = 1) => {
-    obj[key] = obj[key] || 0;
-    obj[key] += increment;
-  }
-
-  server.get('/villes/:city', async (req, res, next) => {
-    const city = req.params.city.toLowerCase();
-    console.log(">>> getting candidates for", city);
-    const csv = await api.getJSONFromCSVFile('candidates_with_cumuleo');
-    console.log(">>> loading", csv.length, "rows from candidates_with_cumuleo.csv");
-    const lists = {};
-    const candidates = csv.filter(r => {
-      if (r.city.toLowerCase() !== city) return false;
-      // console.log(">>> keeping", r.firstname, r.lastname, r.list, r.zipcode, r.city);
-      lists[r.list] = lists[r.list] || { candidates: [], totalPoliticians: 0, totalCumuls: 0, totalYearsInPolitics: 0 };
-      lists[r.list].candidates.push(r);
-      if (!lists[r.list].party) {
-        lists[r.list].party = r.party;
-      }
-      if (r.cumuleo_url) {
-        inc(lists[r.list], 'totalPoliticians');
-        inc(lists[r.list], 'totalCumuls', parseInt(r.cumuls_2017));
-        for (let i = 2004; i < 2016; i++ ) {
-          if (r[i]) {
-            inc(lists[r.list], 'totalYearsInPolitics');
-          }
-        }
-      }
-      return true;
-    });
-    req.data = {
-      city: titleCase(city),
-      lists
-    };
-    console.log(">>> data", req.data);
-    next();
-  });
-
-  server.get('/villes/:city/:listname', async (req, res, next) => {
-    const city = req.params.city.toLowerCase();
-    const listname = req.params.listname.toLowerCase();
-    console.log(">>> getting candidates for", city, listname);
-    const csv = await api.getJSONFromCSVFile('candidates_with_cumuleo');
-    const list = { name: listname, candidates: [] };
-    const stats = { totalPoliticians: 0, totalCumuls: 0, parties: {} };
-    const candidates = csv.filter(r => {
-      if (r.city.toLowerCase() !== city) return false;
-      if (r.list.toLowerCase() !== listname) return false;
-      console.log(">>> keeping", r.firstname, r.lastname, r.list, r.zipcode, r.city, r.cumuleo_url);
-      if (r.cumuleo_url) {
-        inc(stats, 'totalPoliticians');
-        inc(stats, 'totalCumuls', parseInt(r.cumuls_2017));
-        inc(stats.parties, r.party);
-        r.politicalYears = 0;
-        for (let i = 2004; i < 2016; i++ ) {
-          if (r[i]) {
-            inc(stats, 'totalYearsInPolitics');
-            r.politicalYears++;
-          }
-        }
-      }
-      list.candidates.push(r);
-      return true;
-    });
-    list.candidates.sort((a, b) => (parseInt(a.position) > parseInt(b.position) ? 1 : -1));
-    // we define list.party using using the first politician on the list
-    list.candidates.map(c => {
-      if (!list.party) {
-        list.party = c.party;
-      }
-    })
-    req.data = {
-      city: titleCase(city),
-      list,
-      stats
-    };
-    console.log(">>> data.stats", req.data.stats);
-    next();
   });
 
   server.use('/static', (req, res, next) => {
